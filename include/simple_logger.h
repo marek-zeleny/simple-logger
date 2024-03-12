32e64654d4932cdb1bfc906fb7a5ee20689e81ea
Marek Zeleny <zeleny.marek@seznam.cz>
2024-03-12 02:32:22 +0100

    Rename for simplicity and consistency, improve docstrings

--- include/simple_logger.h
+++ include/simple_logger.h
@@ -63,7 +63,8 @@ public:
      *
      * Only logs with equal or higher level will be printed by the logger, lower-level logs will be ignored.
      * The first value (if NDEBUG is set) is for RELEASE builds, the second value is for DEBUG builds.
-     * (NDEBUG is automatically set if CMake is used, otherwise feel free to use any other macro available)
+     * (NDEBUG is automatically set if the appropriate compiler flag is used, otherwise feel free to use any other macro
+     * available)
      */
 #ifdef NDEBUG
     static constexpr LogLevel logLevel{LogLevel::Info};
@@ -116,47 +117,48 @@ private:
 };
 
 /**
- * Logger class intended to be used as a temporary object for each log message.
+ * Log class intended to be used as a temporary object for each log message.
  *
  * You can either use this class directly, or use the convenience macros defined later for less verbose usage.
  * @tparam Level Verbosity level of the log message (the message will be ignored if Config's log level is higher)
  */
 template<LogLevel Level>
-class Logger {
+class Log {
 public:
-    explicit Logger(std::ostream &stream, const std::source_location location = std::source_location::current()) :
-            m_stream(is_active ? stream : null_stream) {
-        if constexpr (is_active) {
+    explicit Log(std::ostream &stream = Config::getDefaultStream<Level>(),
+            const std::source_location location = std::source_location::current()) :
+            m_stream(isActive ? stream : nullStream) {
+        if constexpr (isActive) {
             *this << "[";
-            print_time();
+            printTime();
             *this << "][" << logLevelToString(Level) << "][";
-            print_file_name(location.file_name());
+            printFileName(location.file_name());
             *this << ":" << location.line() << "]";
             *this << "[" << location.function_name() << "] ";
         }
     }
 
-    ~Logger() {
-        if constexpr (is_active) {
+    ~Log() {
+        if constexpr (isActive) {
             m_stream << std::endl;
         }
     }
 
-    std::ostream &get_stream() {
+    std::ostream &getStream() {
         return m_stream;
     }
 
     template<typename T>
-    Logger &operator<<(const T &token) {
-        if constexpr (is_active) {
+    Log &operator<<(const T &token) {
+        if constexpr (isActive) {
             m_stream << token;
         }
         return *this;
     }
 
 private:
-    static constexpr bool is_active{Level >= Config::logLevel};
-    static inline std::ostream null_stream{nullptr};
+    static constexpr bool isActive{Level >= Config::logLevel};
+    static inline std::ostream nullStream{nullptr};
     std::ostream &m_stream;
 
     /**
@@ -164,14 +166,14 @@ private:
      *
      * Using the STL's timezone-supporting implementation and format strings would slow down the logging by a lot.
      */
-    void print_time() {
+    void printTime() {
         auto now = std::chrono::high_resolution_clock::now();
-        auto time_since_epoch = now.time_since_epoch();
-        auto h = std::chrono::duration_cast<std::chrono::hours>(time_since_epoch).count() % 24
+        auto timeSinceEpoch = now.time_since_epoch();
+        auto h = std::chrono::duration_cast<std::chrono::hours>(timeSinceEpoch).count() % 24
                 + Config::timezoneAdjustment;
-        auto min = std::chrono::duration_cast<std::chrono::minutes>(time_since_epoch).count() % 60;
-        auto s = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch).count() % 60;
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(time_since_epoch).count() % 100;
+        auto min = std::chrono::duration_cast<std::chrono::minutes>(timeSinceEpoch).count() % 60;
+        auto s = std::chrono::duration_cast<std::chrono::seconds>(timeSinceEpoch).count() % 60;
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(timeSinceEpoch).count() % 100;
 
         *this << std::setfill('0') << std::setw(2) << h << ":"
               << std::setfill('0') << std::setw(2) << min << ":"
@@ -179,12 +181,12 @@ private:
               << std::setfill('0') << std::setw(3) << ms;
     }
 
-    void print_file_name(const char* file_path) {
-        const char* slash_position = std::strrchr(file_path, '/');
-        if (slash_position != nullptr) {
-            *this << slash_position + 1;
+    void printFileName(const char* filePath) {
+        const char* slashPosition = std::strrchr(filePath, '/');
+        if (slashPosition != nullptr) {
+            *this << slashPosition + 1;
         } else {
-            *this << file_path;
+            *this << filePath;
         }
     }
 };
@@ -201,8 +203,7 @@ private:
 /**
  * Log message on a given level to default output stream with a single stream chain.
  */
-#define SIMPLE_LOGGER_LOG(level) simple_logger::Logger<simple_logger::LogLevel::level>( \
-        simple_logger::Config::getDefaultStream<simple_logger::LogLevel::level>())
+#define SIMPLE_LOGGER_LOG(level) simple_logger::Log<simple_logger::LogLevel::level>()
 
 /**
  * Log a debug message with a single stream chain.
@@ -225,34 +226,31 @@ private:
 #define LOG_ERROR SIMPLE_LOGGER_LOG(Error)
 
 /**
- * Create a local instance of the logger on a given level and get the logger's default stream as a variable of given
- * name.
+ * Create a local instance of a log on a given level and get the logger's default stream as a variable of given name.
  *
  * Use this macro (or derived macros) for more detailed log message control, e.g. giving the stream as a function
  * argument.
  */
 #define GET_LOG_STREAM(level, name) \
-    simple_logger::Logger<simple_logger::LogLevel::level> _sl_logger( \
-            simple_logger::Config::getDefaultStream<simple_logger::LogLevel::level>()); \
-    std::ostream &name = _sl_logger.get_stream()
+    simple_logger::Log<simple_logger::LogLevel::level> _sl_log{}; std::ostream &name = _sl_log.getStream()
 
 /**
- * Create a local instance of a debug logger and get the logger's default stream as a variable of given name.
+ * Create a local instance of a debug log and get the logger's default stream as a variable of given name.
  */
 #define GET_LOG_STREAM_DEBUG(name) GET_LOG_STREAM(Debug, name)
 
 /**
- * Create a local instance of an info logger and get the logger's default stream as a variable of given name.
+ * Create a local instance of an info log and get the logger's default stream as a variable of given name.
  */
 #define GET_LOG_STREAM_INFO(name) GET_LOG_STREAM(Info, name)
 
 /**
- * Create a local instance of a warning logger and get the logger's default stream as a variable of given name.
+ * Create a local instance of a warning log and get the logger's default stream as a variable of given name.
  */
 #define GET_LOG_STREAM_WARNING(name) GET_LOG_STREAM(Warning, name)
 
 /**
- * Create a local instance of an error logger and get the logger's default stream as a variable of given name.
+ * Create a local instance of an error log and get the logger's default stream as a variable of given name.
  */
 #define GET_LOG_STREAM_ERROR(name) GET_LOG_STREAM(Error, name)
 
